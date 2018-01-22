ac08e2387e562b24fa198d9a3ef8b8fcd1b641a1
dury10 <n.nadir@gmail.com>
2018-01-22 19:24:34 +0000

    script fixed

--- xmg_linux.sh
+++ xmg_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 #2018-01-22
 #Made by dury10 Fork from Philipp
 #donations: XMG - 9KcKx8TdnNStNkp7qpjzJZ8CyDTs1RiGqF
@@ -11,10 +11,13 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 follow () {
-	echo "Follow us on Tweeter: @nadirdury & @Coin_Magi_XMG"
+	echo -e "\e[1;36m Follow us on Tweeter: @nadirdury  &  @Coin_Magi_XMG \e[0m"
 }
 slack () {
-	echo "Join owr Slack Community http://slack.m-core.org/"
+	echo -e "\e[1;35m Join owr Slack Community: http://slack.m-core.org/ \e[0m"
+}
+donate () {
+	echo -e "\e[1;33m If you found this script useful please donate :) XMG - 9KcKx8TdnNStNkp7qpjzJZ8CyDTs1RiGqF and we will make more! Thanks! \e[0m"
 }
 
 folder () { 
@@ -48,12 +51,12 @@ compilex86_64 () {
 
                                 DEBIAN_FRONTEND=noninteractive apt-get -y -qq update > /dev/null 2>&1
                                 DEBIAN_FRONTEND=noninteractive apt-get -y -qq upgrade > /dev/null 2>&1
-                                DEBIAN_FRONTEND=noninteractive apt-get -y -qq install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev > /dev/null 2>&1
+                                DEBIAN_FRONTEND=noninteractive apt-get -y -qq install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git pthreads zlib  build-essential zlib1g-dev libncurses5-dev curl > /dev/null 2>&1
                                 git clone https://github.com/magi-project/m-cpuminer-v2 > /dev/null 2>&1
                                 cd m-cpuminer-v2
-                                ./autogen.sh > /dev/null 2>&1
-                                ./configure CFLAGS="-O3" CXXFLAGS="-O3" > /dev/null 2>&1
-                                make clean > /dev/null 2>&1 &&  make -j > /dev/null 2>&1
+                                sudo ./autogen.sh > /dev/null 2>&1
+                                sudo ./configure CFLAGS="-O3" CXXFLAGS="-O3" > /dev/null 2>&1
+                                sudo make clean > /dev/null 2>&1 && sudo  make -j > /dev/null 2>&1
                                 echo "./m-cpuminer-v2"
 
 
@@ -88,28 +91,29 @@ compilearmv71 () {
            [yY]* ) 
 				DEBIAN_FRONTEND=noninteractive apt-get -y -qq update > /dev/null 2>&1
                                 DEBIAN_FRONTEND=noninteractive apt-get -y -qq upgrade > /dev/null 2>&1
-                                DEBIAN_FRONTEND=noninteractive apt-get -y -qq install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev  > /dev/null 2>&1
+                                DEBIAN_FRONTEND=noninteractive apt-get -y -qq install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git pthreads zlib  build-essential zlib1g-dev libncurses5-dev curl > /dev/null 2>&1
                                 git clone https://github.com/magi-project/m-cpuminer-v2 > /dev/null 2>&1
                                 cd m-cpuminer-v2
-                                ./autogen.sh > /dev/null 2>&1
-                                CFLAGS="-O2 mfpu=neon-vfpv4" ./configure > /dev/null 2>&1
-				read -p "are using a Raspberry. Yes/No [yY/nN]" pi
-	case $pi in
-	[yY]* )
-				sed -i 's/-march=native/-mcpu=cortex-a53/g' Makefile.am
+                                sudo ./autogen.sh > /dev/null 2>&1
+                                sudo CFLAG="-O2 mfpu=neon-vfpv4" ./configure > /dev/null 2>&1
+				read -p "Are you using Raspberry. Yes/No [yY/nN]" pi
+	  case $pi in
+	  [yY]* )
+				sudo sed -i 's/-march=native/-mcpu=cortex-a53/g' Makefile
+				sudo sed -i 's/-march=mative/-mcpu=cortex-a53/g' m7/Makefile
 				echo "The cpu detection was set to cortex-a53"
 		  break;;
-	[nN]* )
-				echo "You are a luck one :D"
+	  [nN]* )
+				echo "The cpu detection it's set to native"
 
-                                make clean > /dev/null 2>&1 &&  make -j4 > /dev/null 2>&1
-                                echo "./m-cpuminer-v2"
 		  break;;
-	* )
-		echo "Yex/No [yY/nN]";;
- 
-	esac
-		 break;;
+	  * )    echo "Yes/No [yY/nN]";;
+	  esac
+
+                                sudo make clean > /dev/null 2>&1 && sudo  make -j4 > /dev/null 2>&1
+                                echo "./m-cpuminer-v2"
+                  break;;
+
            [nN]* )
                                 read -p "Thanks, to exit the install pres Ctrl+C"
                   break;;
@@ -137,27 +141,27 @@ compileaarch64 () {
            [yY]* )  
 				DEBIAN_FRONTEND=noninteractive apt-get -y -qq update > /dev/null 2>&1
                                 DEBIAN_FRONTEND=noninteractive apt-get -y -qq upgrade > /dev/null 2>&1
-                                DEBIAN_FRONTEND=noninteractive apt-get -y -qq install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev > /dev/null 2>&1
+                                DEBIAN_FRONTEND=noninteractive apt-get -y -qq install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git pthreads zlib  build-essential zlib1g-dev libncurses5-dev curl > /dev/null 2>&1
                                 git clone https://github.com/magi-project/m-cpuminer-v2 > /dev/null 2>&1
                                 cd m-cpuminer-v2
-                                ./autogen.sh > /dev/null 2>&1
-                                CFLAGS="-O2 mfpu=neon-vfpv4" ./configure > /dev/null 2>&1
-		                read -p "Are using a Raspberry. Yes/No [yY/nN]" pi
-        case $pi in
-        [yY]* )
-                                sed -i 's/-march=native/-mcpu=cortex-a53/g' Makefile.am
-                                echo "The cpu detection was set to cortex-a53"
-                  break;;
-        [nN]* )
-                                echo "You are a luck one :D"
-                                make clean > /dev/null 2>&1 &&  make -j4 > /dev/null 2>&1
-                                echo "./m-cpuminer-v2"
+                                sudo ./autogen.sh > /dev/null 2>&1
+                                sudo CFLAG="-O2 mfpu=neon-vfpv4" ./configure > /dev/null 2>&1
+				read -p "Are you using a Raspberry. Yes/No [yY/nN]" pi
+	 case $pi in
+	  [yY]* )
+				sudo sed -i 's/-march=native/-mcpu=cortex-a53/g' Makefile
+				sudo sed -i 's/-march=native/-mcpu=cortex-a53/g' m7/Makefile
+				eco "The cpu detection was set to cortex-a53"
 		  break;;
-           * )
-                echo "Yex/No [yY/nN]";;
+	  [nN]* )
+				echo "The cpu detection it's native"
+		  break;;
+	  * ) 	 echo "Yes/No [yY/nN]";;
+	 esac
 
-        esac
-                 break;;
+                                sudo make clean > /dev/null 2>&1 && sudo  make -j4 > /dev/null 2>&1
+                                echo "./m-cpuminer-v2"
+                  break;;
 
            [nN]* )
                                 read -p "Thanks, to exit the install pres Ctrl+C"
@@ -204,20 +208,12 @@ runme () {
 
 poolcreds () {
 
-			echo "Let's set up the pool details"
-			echo "Enter the pool url, for excemple: xmg.minerclaim.net"
-                        read -p "poolurl: " poolurl 
-			echo "Enter the pool port, normally its a 4 digit nr, for example: 3333" 
-			read -p "poolport: " poolport
-			echo "Enter your pool username"
+                        read -p "poolurl: " poolurl
+                        read -p "poolport: " poolport
                         read -p "username: " username
-			echo "Enter your worker name"
                         read -p "workername: " workername
-			echo "Enter your worker password"
                         read -p "workerpass: " workerpass
-			echo "Enter how many thread you want to mine with, if your processor have 4 cores you can run 3 for mining and 1 for browsing or you can run all 4 cores" 
                         read -p "threads: " threads
-			echo "Enter ho much % of your processor you want to be used for mining, for exemple if you want to use 80% type 80"
                         read -p "cpu_efficiency: "  cpu_efficiency
                         string="./m-minerd -a m7mhash -o stratum+tcp://$poolurl:$poolport -u $username.$workername -p $workerpass -t $threads -e $cpu_efficiency"
                         echo $string
@@ -250,44 +246,39 @@ case $type in
 		folder $default
 		path=$(compilex86_64 $default)
 		runme $default $path
-		echo "If you like what I'm doing please donate :) XMG - 9KcKx8TdnNStNkp7qpjzJZ8CyDTs1RiGqF"
 		follow
 		slack
+		donate
 		exit 0;;
        
    armv71)
 		folder $default
 		path=$(compilearmv71 $default)
 		runme $default $path
-		echo "If you like what I'm doing please donate :) XMG - 9KcKx8TdnNStNkp7qpjzJZ8CyDTs1RiGqF"
 		follow
 		slack
+		donate 
 		exit 0;;
 	  
-
-  armv7l)
-		folder $default
-		path=$(compilearmv71 $default)
-		runme $default $path
-		echo "If you like what I'm doing please donate :) XMG - 9KcKx8TdnNStNkp7qpjzJZ8CyDTs1RiGqF"
-		follow
-		slack
-		exit 0;;
-
+   armv7l)
+                folder $default
+                path=$(compilearmv71 $default)
+                runme $default $path
+                follow
+                slack
+                donate
+                exit 0;;
 
    aarch64)
 		folder $default
 		path=$(compileaarch64 $default)
 		runme $default $path
-		echo "If you like what I'm doing please donate :) XMG - 9KcKx8TdnNStNkp7qpjzJZ8CyDTs1RiGqF"
 		follow
 		slack
+		donate
 		exit 0;;
 		
   * )   echo "Your CPU Architecture is not supported. Sorry"
-	follow
-	slack
-		
-	exit 1;;
+		exit 1;;
      
 esac 
